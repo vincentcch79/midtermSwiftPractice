d57e0df4c27d4bb16c25511ca3b81c03906a31b7
Vincent Chang <vincentcch79@gmail.com>
2016-07-02 20:51:05 +0800

    0702 final update

--- midTermExamPlayground.playground/Contents.swift
+++ midTermExamPlayground.playground/Contents.swift
@@ -1,60 +1,37 @@
-//: Playground - noun: a place where people can play
+////: Playground - noun: a place where people can play
 
 
 
-var arr1 = [0,9,100,4000,23123,123,9]
-var arr2 = [5,0,90,44,3]
-var arr3 = arr1 + arr2
-var arr4 = Array(Set(arr3))
-
-func merge(leftArray leftArray: [Int], rightArray: [Int]) -> [Int] {
+func sortArray(array1: [Int], array2: [Int]) -> [Int] {
     
-    var leftIndex = 0
-    var rightIndex = 0
+    var mergeArray = array1 + array2
+    var setArray = Array(Set(mergeArray))
+    var SortedArray: [Int] = []
+    var a = setArray
     
-    var mergeArray = [Int]()
-    
-    while leftIndex < leftArray.count && rightIndex < rightArray.count {
-        if leftArray[leftIndex] < rightArray[rightIndex] {
-            mergeArray.append(leftArray[leftIndex])
-                        leftIndex += 1
-        } else if leftArray[leftIndex] > rightArray[rightIndex] {
-            mergeArray.append(rightArray[rightIndex])
-            rightIndex += 1
-        } else {
-            mergeArray.append(leftArray[leftIndex])
-            leftIndex += 1
-            mergeArray.append(rightArray[rightIndex])
-            rightIndex += 1
-            
+    for x in 1..<a.count {
+        var y = x
+        let temp = a[y]
+        while y > 0 && temp < a[y - 1] {
+            a[y] = a[y - 1]
+            y -= 1
         }
+        a[y] = temp
     }
     
+    
+    
+//    return SortedArray
+    
+    return a
 
-    while leftIndex < leftArray.count {
-        mergeArray.append(leftArray[leftIndex])
-        leftIndex += 1
-    }
-    while rightIndex < rightArray.count {
-        mergeArray.append(rightArray[rightIndex])
-        rightIndex += 1
-    }
-    
-    return mergeArray
 }
 
-func sortArray(array: [Int]) -> [Int] {
+sortArray([0,0,4,7,89,3,2], array2: [5,4,100,55,99])
 
-    guard array.count > 1 else { return array }
-        let middleIndex = array.count / 2
-    
-    let leftArray = sortArray(Array(array[0..<middleIndex]))
-    let rightArray = sortArray(Array(array[middleIndex..<array.count]))
-    
-    return merge(leftArray: leftArray, rightArray: rightArray)
-}
 
-sortArray(arr4)
+
+
 
 
 
